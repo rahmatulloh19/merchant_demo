0f05db589deca859adc761ca2a3eb2f2ca11724f
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2025-12-03 14:06:57 +0500

    install react-router-dom and set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import { Button } from "./components/ui/button";
+
 function App() {
-	return <div className="bg-gray-700 flex-grow-1"></div>;
+	return (
+		<div className="container flex-grow-1">
+			<div className="flex flex-col gap-4 h-full justify-center items-center bg-warning bg-white">
+				<NavLink to="/order">
+					<Button>Create Order</Button>
+				</NavLink>
+				<NavLink to="/archive">
+					<Button>Archive</Button>
+				</NavLink>
+			</div>
+
+			<Routes>
+				<Route path="/order" />
+				<Route path="/archive" />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
